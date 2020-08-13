5cdb4e30f59d75c3147131621801d61c1e2f9017
Nguyễn Thị Lan Hương <43417721+huongcnttnb@users.noreply.github.com>
2020-08-13 23:53:12 +0700

    Create Circle.java

---
+++ Week4/src/Week4/Circle.java
@@ -0,0 +1,34 @@
+package Week4;
+
+public class Circle {
+	protected final double PI = 3.14;
+	private double radius = 1.0;
+	private String color = "red";
+	public Circle() {
+	}
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	public Circle(double radius, String color) {
+		this.radius = radius;
+		this.color = color;
+	}
+	public double getRadius() {
+		return this.radius;
+	}
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	public String getColor() {
+		return this.color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public double getArea() {
+		return PI * this.radius * this.radius;
+	}
+	public String toString() {
+		return "Circle[radius=" + this.radius + ",color=" + this.color + "]";
+	}
+}
